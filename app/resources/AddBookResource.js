674104877f3fd0a9f1b898c55756a25be9c0cf3f
edvinpontuslarsson <el223na@student.lnu.se>
2018-03-15 11:00:23 +0100

    Update AddBookResource.js

--- app/resources/AddBookResource.js
+++ app/resources/AddBookResource.js
@@ -2,10 +2,27 @@
     "use strict";
 
     var LibraryDAO = require('../dao/LibraryDAO');
+    // var util = require('util');
 
 
     module.exports = function (data, callback) {
-        
+        var filePromise = LibraryDAO.readXMLFile();
+    
+        filePromise.then(function(bookInfo) {
+            // console.log(util.inspect(bookInfo, false, null));
+            var index = bookInfo.catalog.book.length -1;
+            var tempId = bookInfo.catalog.book[index].$.id;
+            var id = {
+                id: tempId + 1
+            }; 
+            data.$ = id;
+
+            bookInfo.catalog.book.push(data);
+
+            LibraryDAO.writeXMLFile(bookInfo);
+        });
+
+        callback();
     };
 
 }());
